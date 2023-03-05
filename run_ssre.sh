c4cd8c4c78998673839ac4a77a7ac3b8f101e0b0
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-05 21:29:55 +0800

    set data path

--- run_ssre.sh
+++ run_ssre.sh
@@ -1,2 +1,2 @@
-python main.py --config=./exps/ssre_cifar/ssre_5steps.json
-python main.py --config=./exps/ssre_cifar/ssre_10steps.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_5steps.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_10steps.json
