b1533258e50dd499d5c9c283ef5950eeccf72b97
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-05 21:15:30 +0800

    set data path

--- run_ssre.sh
+++ run_ssre.sh
@@ -1,2 +1,2 @@
-python main.py --config=./exps/ssre/ssre_5steps.json
-python main.py --config=./exps/ssre/ssre_10steps.json
+python main.py --config=./exps/ssre_cifar/ssre_5steps.json
+python main.py --config=./exps/ssre_cifar/ssre_10steps.json
