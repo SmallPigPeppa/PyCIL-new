e63fa4f21a686905041b72da8d6810f66b080171
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-05 23:06:05 +0800

    add pretrain

--- run_pass_c.sh
+++ run_pass_c.sh
@@ -1,4 +1,4 @@
 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/pass_cifar/pass_5steps.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/pass_cifar/pass_10steps.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/pass_cifar/pass_5steps_pretrain.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/pass_cifar/pass_10steps_pretrain.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/pass_cifar/pass_10steps.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/pass_cifar/pass_5steps_pretrain.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/pass_cifar/pass_10steps_pretrain.json
