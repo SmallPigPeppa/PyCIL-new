82a5f3b9b837e16333a617697d437a8dc5f2c1c6
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-05 21:35:22 +0800

    add pretrain

---
+++ run_ssre_pretrain.sh
@@ -0,0 +1,2 @@
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_5steps_pretrain.json
+/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_10steps_pretrain.json
