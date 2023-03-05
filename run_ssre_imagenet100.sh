b4048279bf4f749a2024b879272bb7ff7f5d7ee9
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2023-03-05 22:23:14 +0800

    add pretrain

--- run_ssre_imagenet100.sh
+++ run_ssre_imagenet100.sh
@@ -1,4 +1,4 @@
 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_10steps.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps_pretrain.json
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_10steps_pretrain.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_10steps.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps_pretrain.json
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_10steps_pretrain.json
