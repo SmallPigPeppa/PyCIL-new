48d5ea5993fb77c5289d4fbc241bb5b039edb023
lwz <905328134@qq.com>
2023-11-28 23:01:58 +0800

    add pretrain

--- run_ssre_semi.sh
+++ run_ssre_semi.sh
@@ -1,7 +1,7 @@
-#CUDA_VISIBLE_DEVICES=0 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_5steps.json &
-#CUDA_VISIBLE_DEVICES=1 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_10steps.json &
-#CUDA_VISIBLE_DEVICES=2 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_5steps_pretrain.json &
-#CUDA_VISIBLE_DEVICES=3 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_10steps_pretrain.json
+CUDA_VISIBLE_DEVICES=0 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_5steps.json &
+CUDA_VISIBLE_DEVICES=1 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_10steps.json &
+CUDA_VISIBLE_DEVICES=2 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_5steps_pretrain.json &
+CUDA_VISIBLE_DEVICES=3 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_cifar/ssre_10steps_pretrain.json &
 CUDA_VISIBLE_DEVICES=4 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps.json &
 CUDA_VISIBLE_DEVICES=5 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_10steps.json &
 CUDA_VISIBLE_DEVICES=6 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps_pretrain.json &
