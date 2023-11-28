b068df8ac70c47a45dcf1ba75709cda0b1290b4e
lwz <905328134@qq.com>
2023-11-28 22:52:42 +0800

    add pretrain

--- run_ssre_i.sh
+++ run_ssre_i.sh
@@ -1,4 +1,9 @@
-/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps.json
+CUDA_VISIBLE_DEVICES=4 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps.json &
+CUDA_VISIBLE_DEVICES=5 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_10steps.json &
+CUDA_VISIBLE_DEVICES=6 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps_pretrain.json &
+CUDA_VISIBLE_DEVICES=7 /root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_10steps_pretrain.json
+
+#/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps.json
 #/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_10steps.json
 #/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_5steps_pretrain.json
 #/root/miniconda3/envs/torch/bin/python main.py --config=./exps/ssre_imagenet100/ssre_10steps_pretrain.json
