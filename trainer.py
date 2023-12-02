9679a70865884922410fded01912a54a2db1c15c
lwz <905328134@qq.com>
2023-12-02 22:35:06 +0800

    add pretrain

--- trainer.py
+++ trainer.py
@@ -26,7 +26,7 @@ def _train(args):
     if not os.path.exists(logs_name):
         os.makedirs(logs_name)
 
-    logfilename = "logs/{}/_{}_{}_{}_{}_{}_{}".format(
+    logfilename = "logs/{}/{}/{}/{}/{}_{}_{}".format(
         args["model_name"],
         args["dataset"],
         init_cls,
